53aae24e96ea0048164261e0e8bcb82906bbce7f
Ricardo S. Chao <richiechao95@gmail.com>
2019-10-26 17:02:49 +0800

    [Update] remove unused code

--- FileManagement.py
+++ FileManagement.py
@@ -13,25 +13,21 @@ class File:
     def get_file(self):
         # edit file path to read in the file
         try:
-            file_path = self.file_path.replace("\\", "/").replace("\"", "")
-            file = open(file_path, mode="r")
-            split_file_path = file_path.split("/")
+            self.file_path = self.file_path.replace("\\", "/").replace("\"", "")
+            file = open(self.file_path, mode="r")
+            split_file_path = self.file_path.split("/")
             # Collect title (activity name), and create temp_data for further processing
             temp_data = open("./temp_data.csv", mode="w+")
             for index, line in enumerate(file):
                 if index == 0:
                     activity_name = line.split(",")[0]
-                    # Extract 1st Category
-                    first_cat = line.split(" ")[-1].split(",")[0]
-                    # Extract 2nd Category
-                    second_cat = line.split("】")[1].split("/")[0]
                     print("目前處理活動: ", activity_name)
                 else:
                     temp_data.write(line)
             file.close()
             temp_data.close()
             print("========== 檔案抓取成功，程式繼續 ==========")
-            return file_path, first_cat, second_cat
+            #return file_path
         except BaseException:
             print("========== 檔案抓取失敗，程式繼續 ==========")
             print("您所輸入的路徑不正確或不存在，程式終止")
@@ -59,3 +55,9 @@ def remove_temp():
         os_remove("./temp_data.csv")
     except:
         print("Could not find temp file, program exit.")
+
+
+# "C:\Users\ricardo\Desktop\Data\0311_藍天百腦匯報名清單(登陸出席).csv"
+file_path = input()
+kkk = File(file_path, "107-2", "TIP", "藍天電腦", "2019/1/1")
+
