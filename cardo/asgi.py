25fc1edf6029404685acdfbfe61c1a89a8925fdb
chaoannricardo <richiechao95@gmail.com>
2020-01-21 15:12:27 +0800

    [new] recreate django structure

--- cardo/asgi.py
+++ cardo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DatabaseWebform project.
+ASGI config for cardo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DatabaseWebform.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cardo.settings')
 
 application = get_asgi_application()
