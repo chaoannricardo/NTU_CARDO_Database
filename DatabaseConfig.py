93ce2e6da4c69e08d5dfbcc15a289b6b801e1105
Ricardo S. Chao <richiechao95@gmail.com>
2020-01-25 15:21:28 +0800

    [up] edit configuration file

--- DatabaseConfig.py
+++ DatabaseConfig.py
@@ -1,7 +1,7 @@
 def get_config(account, password):
     config = {
                     #'host': '127.0.0.1',
-                    'host': '192.168.92.132',
+                    'host': '220.135.181.17',
                     'port': 3306,
                     'user': account,
                     'password': password,
