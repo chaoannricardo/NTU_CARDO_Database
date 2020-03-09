50af2fb15c1fc000c0c3afe6ff2fde2e8d9b7e57
Ricardo S. Chao <richiechao95@gmail.com>
2020-03-09 11:45:50 +0800

    [up] configuration updated

--- __init__.py
+++ __init__.py
@@ -175,8 +175,8 @@ def admin_control():
 def get_config(account, password):
     config = {
                     #'host': '127.0.0.1',
-                    'host': '220.133.208.39',
-                    #'host': '10.181.2.122',
+                    #'host': '220.133.208.39',
+                    'host': '10.181.2.122',
                     'port': 3306,
                     'user': account,
                     'password': password,
