b572fe336d719e3c2ba109ceb214e80a67948910
Ricardo S. Chao <richiechao95@gmail.com>
2020-04-26 20:17:55 +0800

    [up] log in panel amendment

--- view.py
+++ view.py
@@ -1,5 +1,5 @@
+from pymysql import cursors
 from tkinter import *
-from time import sleep as t_sleep
 import database_management
 import tkinter.font as tkFont
 
@@ -17,6 +17,7 @@ def get_license():
 
 
 def get_config(account, password):
+    global config
     config = {
                     # 'host': '127.0.0.1',
                     'host': '220.133.208.39',
@@ -28,26 +29,38 @@ def get_config(account, password):
                     'charset': 'utf8mb4',
                     'cursorclass': cursors.DictCursor,
                 }
-    return config
 
 
-def log_in(account, password):
+def destroy_frame(frame):
+    frame.destroy()
+
+
+def log_in(account_entry, password_entry):
     try:
-        config = get_config(account, password)
-        # 身分驗證
-        print('# 登入中....')
+        account = account_entry.get()
+        password = password_entry.get()
+        get_config(account, password)
         conn = database_management.pymysql_connect(**config)
-        print("# 登入成功，歡迎回來", account)
-        t_sleep(1)
     except:
-        print("# 您輸入的帳號或密碼錯誤，請再輸入一次。")
-        print()
-        print()
-    return config
+        # clean up caution frame if there's anything inside
+        if "caution_frame" in locals():
+            destroy_frame(caution_frame)
+        else:
+            caution_frame = Frame(win)
+
+        # delete config if log in process is not completed.
+        if "config" in globals():
+            del config
+
+
+        incorrect_font_style = tkFont.Font(family="微軟正黑體", size=14, weight=tkFont.BOLD)
+        incorrect_message_label = Label(caution_frame, text="您所輸入的帳號密碼不正確，請再輸入一次",
+                                        font=incorrect_font_style, fg='red').pack()
 
 
 if __name__ == '__main__':
     # construct a window
+    global win
     win = Tk()
     font_style = tkFont.Font(family="微軟正黑體", size=14)
     win.title("NTU CARDO 資料庫管理系統")
@@ -58,38 +71,31 @@ if __name__ == '__main__':
     main_frame.pack(side=TOP, fill=X, padx=5)
 
     # create a text label
-    main_text = "----------------------------------------------------------------------------------\n" + \
-                "【國立臺灣大學管理學院生涯發展中心（CARDO）資料處理及資料庫管理程式】\n"
-    main_text_label = Label(text=main_text, font=font_style)
-    main_text_label.pack()
+    main_text = "\n【國立臺灣大學管理學院生涯發展中心（CARDO）資料處理及資料庫管理程式】"
+    main_text_label = Label(main_frame, text=main_text, font=font_style).pack()
 
     # licence label
     license_font_style = tkFont.Font(family="微軟正黑體", size=8)
     license_text = get_license()
-    license_label = Label(text=license_text, font=license_font_style)
-    license_label.pack()
+    license_label = Label(main_frame, text=license_text, font=license_font_style).pack()
 
     # create log in column
-    account_label = Label(text="請輸入帳號：", font=font_style)
-    account_label.pack()
-    account_entry = Entry(font=font_style)
-    account_entry.pack()
+    account_label = Label(main_frame, text="請輸入帳號：", font=font_style).pack()
+    account_entry = Entry(main_frame, font=font_style).pack()
 
-    br_label = Label(text="   ")
-    br_label.pack()
+    br_label = Label(main_frame, text="   ").pack()
 
-    password_label = Label(text="請輸入密碼：", font=font_style)
-    password_label.pack()
-    password_entry = Entry(font=font_style)
-    password_entry.pack()
+    password_label = Label(main_frame, text="請輸入密碼：", font=font_style).pack()
+    password_entry = Entry(main_frame, font=font_style).pack()
 
     # create log in button
-    br_label = Label(text="   ")
-    br_label.pack()
-    
-    btn_login = Button(text="登入", font=font_style)
-    btn_login.config(bg='skyblue', width=5, height=1)
-    btn_login.pack()
+    br_label = Label(main_frame, text="   ").pack()
+
+    btn_login = Button(main_frame, text="登入", font=font_style, bg='skyblue', width=5, height=1,
+                       command=lambda: log_in(account_entry, password_entry)).pack()
+
+
+
 
 
     win.mainloop()
